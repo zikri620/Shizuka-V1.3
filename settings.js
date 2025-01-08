67b41eb787a72829e15aa7211fb30f3bb84dfb78
zikri620 <habibiemzikri@gmail.com>
2025-01-08 08:52:19 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -16,27 +16,27 @@ global.btz = 'masukkan apikey mu di sini'
 //===============SETTING MENU==================\\
 global.thumbnail = fs.readFileSync("./data/image/thumb.jpg")
 global.ig = '--'
-global.yt = 'Tan'
+global.yt = '-'
 global.ttowner = '@codenametan'
-global.ownername = '@Tan'
-global.owner = ['72722727272'] // SETTING JUGA DI FOLDER DATABASE OWNER.json
-global.ownernomer = '1718272828'
+global.ownername = 'Zikri'
+global.owner = ['6285363815018'] // SETTING JUGA DI FOLDER DATABASE OWNER.json
+global.ownernomer = '6285363815018'
 global.socialm = 'GitHub: Tanmyname'
 global.location = 'Indonesia' 
 //========================setting Payment=====================\\
-global.nodana = '' // KOSONG KAN JIKA TIDAK ADA
+global.nodana = '081266507998' // KOSONG KAN JIKA TIDAK ADA
 global.nogopay = '' // KOSONG KAN JIKA TIDAK ADA 
 global.noovo = '' // KOSONG KAN JIKA TIDAK ADA
 //==================setting Payment Name===========================\\
-global.andana = 'Tan' // KOSONG KAN JIKA TIDAK ADA
-global.angopay = 'Tan' // KOSONG KAN JIKA TIDAK ADA
+global.andana = 'Zikri' // KOSONG KAN JIKA TIDAK ADA
+global.angopay = '-' // KOSONG KAN JIKA TIDAK ADA
 global.anovo = '' // KOSONG KAN JIKA TIDAK ADA
 //==================setting bot===========================\\
-global.botname = "Tv2"
-global.ownernumber = '1818191'
-global.botnumber = '6285198468895'
+global.botname = "ZikriBot"
+global.ownernumber = '6285363815018'
+global.botnumber = '6287874298171'
 global.ownername = '@Tan'
-global.ownerNumber = ["61827191818@s.whatsapp.net"]
+global.ownerNumber = ["6285363815018@s.whatsapp.net"]
 global.ownerweb = ""
 global.websitex = ""
 global.gris = '`'
@@ -46,8 +46,8 @@ global.themeemoji = '🪀'
 global.wm = "Tv2 | 6285198468895"
 global.botscript = 'Ngk ada wle😂' //script link
 global.packname = "Sticker By"
-global.author = "\n\n\n\n\nCreate by Tv2\nNo hape/wa : 6285198468895"
-global.creator = "6285198468895@s.whatsapp.net"
+global.author = "\n\n\n\n\nCreate by Tv2\nNo hape/wa : 6285363815018"
+global.creator = "6287874298171@s.whatsapp.net"
 //======================== CPANEL FITUR ===========================\\
 global.domain = '-' // Isi Domain Lu jangan kasih tanda / di akhir link
 global.apikey = '-' // Isi Apikey Plta Lu
